a88ab99fe6fe48fec39a783f484ec9a0544f1de5
chosun8do2 <chosun8do@naver.com>
2018-03-26 21:34:27 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>웅라어라어라ㅓ아ㅓ아</title>
+        <style type="text/css">
+            input {
+                display:block;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+            <input type="text" value="" placeholder="ID" />
+            <input type="password" value="" placeholder="Password" />
+            <button>로그인</button>
+            <a href="/signup.php" style="text-decoration:underline;color:red; font-size:10px;">회원가입</a>
+        </div>
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>sign up</title>
+    </head>
+    <body>
+        <p> 회원가입 페이지</p>
+    </body>
+</html>
